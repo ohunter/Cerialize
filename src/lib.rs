2d17aaceeae9483d8e08867d529214898b821d1d
Ole-Jeger Hoffstuen <ole.hoffstuen@kd.kongsberg.com>
2023-07-14 17:14:04 +0200

    Moving closer to actually being able to serialize this shit

--- src/lib.rs
+++ src/lib.rs
@@ -1,126 +1,6 @@
 mod types;
 
-use pyo3::exceptions::PyAttributeError;
 use pyo3::prelude::*;
-use pyo3::types::{PyDict, PyLong, PyString, PyType};
-use std::cell::RefCell;
-
-// /// Formats the sum of two numbers as string.
-// #[pyfunction]
-// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-//     Ok((a + b).to_string())
-// }
-
-// #[pyfunction]
-// fn serialize()
-
-#[pyclass(name = "_cstruct", subclass, weakref)]
-struct CStruct {
-    buffer: RefCell<Vec<u8>>,
-}
-
-impl CStruct {
-    fn type_and_offset_of(slf: &PyCell<Self>, attr: String) -> PyResult<(&PyType, usize)> {
-        let fields = slf.getattr("_CFIELDS")?.downcast::<PyDict>()?;
-        let mut attr_type = None;
-
-        let offset = fields
-            .iter()
-            .map_while(|(key, value)| {
-                let cur = key.downcast::<PyString>().unwrap();
-                match cur.to_string() == attr {
-                    false => {
-                        let field_type = value.downcast::<PyType>().unwrap();
-                        Some(
-                            field_type
-                                .call_method0("__packed_size__")
-                                .unwrap()
-                                .downcast::<PyLong>()
-                                .unwrap()
-                                .extract::<usize>()
-                                .unwrap(),
-                        )
-                    }
-                    true => {
-                        attr_type = Some(value.downcast::<PyType>().unwrap());
-                        None
-                    }
-                }
-            })
-            .fold(0_usize, |acc, val| acc + val);
-
-        match attr_type {
-            Some(type_) => Ok((type_, offset)),
-            None => Err(PyAttributeError::new_err(format!(
-                "Unable to locate attribute {attr}"
-            ))),
-        }
-    }
-}
-
-#[pymethods]
-impl CStruct {
-    #[new]
-    fn new(capacity: usize) -> Self {
-        Self {
-            buffer: RefCell::new(vec![0_u8; capacity]),
-        }
-    }
-
-    #[classmethod]
-    fn from_buffer(cls: &PyType, buffer: &[u8]) -> PyResult<Self> {
-        let minimum_size = cls
-            .call_method0("__packed_size__")?
-            .downcast::<PyLong>()?
-            .extract::<usize>()?;
-        Ok(Self::from(&buffer[..minimum_size]))
-    }
-
-    #[classmethod]
-    fn __packed_size__(cls: &PyType) -> PyResult<usize> {
-        let mut capacity = 0_usize;
-        // This assumes that there are no padding bytes
-        // That should probably be fixed at some point
-        let fields = cls.getattr("_CFIELDS")?.downcast::<PyDict>()?;
-        for (_key, value) in fields {
-            let field_type = value.downcast::<PyType>()?;
-            capacity += field_type
-                .call_method0("__packed_size__")?
-                .downcast::<PyLong>()?
-                .extract::<usize>()?;
-        }
-        Ok(capacity)
-    }
-
-    fn _offset_of(slf: &PyCell<Self>, attr: String) -> PyResult<usize> {
-        Ok(Self::type_and_offset_of(slf, attr)?.1)
-    }
-
-    fn _type_of(slf: &PyCell<Self>, attr: String) -> PyResult<&PyType> {
-        Ok(Self::type_and_offset_of(slf, attr)?.0)
-    }
-
-    fn __getattr__(slf: &PyCell<Self>, attr: String) -> PyResult<&PyAny> {
-        let (attr_type, buffer_offset) = Self::type_and_offset_of(slf, attr)?;
-        let type_size = attr_type
-            .call_method0("__packed_size__")?
-            .downcast::<PyLong>()?
-            .extract::<usize>()?;
-
-        Ok(attr_type.call_method1(
-            "from_buffer",
-            (&slf.borrow().buffer.borrow()[buffer_offset..buffer_offset + type_size],),
-        )?)
-    }
-}
-
-impl From<&[u8]> for CStruct {
-    fn from(value: &[u8]) -> Self {
-        Self {
-            buffer: RefCell::new(Vec::from(value)),
-        }
-    }
-}
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -139,6 +19,9 @@ fn cerialize(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<types::Float16>()?;
     m.add_class::<types::Float32>()?;
     m.add_class::<types::Float64>()?;
-    m.add_class::<CStruct>()?;
+    m.add_class::<types::CStruct>()?;
+    m.add_class::<types::Native>()?;
+    m.add_class::<types::Big>()?;
+    m.add_class::<types::Little>()?;
     Ok(())
 }
--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,130 @@
 use half::f16;
-use pyo3::types::PyType;
+use pyo3::exceptions::PyAttributeError;
+use pyo3::types::{PyDict, PyLong, PyString, PyType};
+use pyo3::types::PyBytes;
 use pyo3::{prelude::*, pyclass::CompareOp};
+use std::cell::RefCell;
+
+/// Converts a slice to an array of the specified size
+fn buffer_alias<const N: usize>(buffer: &[u8]) -> &[u8; N] {
+    buffer.try_into().expect("slice with incorrect length")
+}
+
+#[pyclass(weakref)]
+#[derive(Clone)]
+pub struct Native();
+
+#[pyclass(weakref)]
+#[derive(Clone)]
+pub struct Big();
+
+#[pyclass(weakref)]
+#[derive(Clone)]
+pub struct Little();
+
+#[derive(FromPyObject)]
+pub enum Endianness{
+    #[pyo3(transparent)]
+    Native(Native),
+    #[pyo3(transparent)]
+    Big(Big),
+    #[pyo3(transparent)]
+    Little(Little),
+}
+
+#[pyclass(name = "_cstruct", subclass, weakref)]
+pub struct CStruct {
+    buffer: RefCell<Vec<u8>>,
+}
+
+impl CStruct {
+    fn type_and_offset_of(slf: &PyCell<Self>, attr: String) -> PyResult<(&PyType, usize)> {
+        let fields = slf.getattr("_CFIELDS")?.downcast::<PyDict>()?;
+        let mut attr_type = None;
+
+        let offset = fields
+            .iter()
+            .map_while(|(key, value)| {
+                let cur = key.downcast::<PyString>().unwrap();
+                match cur.to_string() == attr {
+                    false => {
+                        let field_type = value.downcast::<PyType>().unwrap();
+                        Some(
+                            field_type
+                                .call_method0("__packed_size__")
+                                .unwrap()
+                                .downcast::<PyLong>()
+                                .unwrap()
+                                .extract::<usize>()
+                                .unwrap(),
+                        )
+                    }
+                    true => {
+                        attr_type = Some(value.downcast::<PyType>().unwrap());
+                        None
+                    }
+                }
+            })
+            .fold(0_usize, |acc, val| acc + val);
+
+        match attr_type {
+            Some(type_) => Ok((type_, offset)),
+            None => Err(PyAttributeError::new_err(format!(
+                "Unable to locate attribute {attr}"
+            ))),
+        }
+    }
+}
+
+#[pymethods]
+impl CStruct {
+    #[new]
+    fn new(buffer: &[u8]) -> Self {
+        Self {
+            buffer: RefCell::new(Vec::from(buffer)),
+        }
+    }
+
+    #[classmethod]
+    fn __packed_size__(cls: &PyType) -> PyResult<usize> {
+        let mut capacity = 0_usize;
+        // This assumes that there are no padding bytes
+        // That should probably be fixed at some point
+        let fields = cls.getattr("_CFIELDS")?.downcast::<PyDict>()?;
+        for (_key, value) in fields {
+            let field_type = value.downcast::<PyType>()?;
+            capacity += field_type
+                .call_method0("__packed_size__")?
+                .downcast::<PyLong>()?
+                .extract::<usize>()?;
+        }
+        Ok(capacity)
+    }
+
+    fn _offset_of(slf: &PyCell<Self>, attr: String) -> PyResult<usize> {
+        Ok(Self::type_and_offset_of(slf, attr)?.1)
+    }
+
+    fn _type_of(slf: &PyCell<Self>, attr: String) -> PyResult<&PyType> {
+        Ok(Self::type_and_offset_of(slf, attr)?.0)
+    }
+
+    fn __getattr__(slf: &PyCell<Self>, attr: String) -> PyResult<&PyAny> {
+        let (attr_type, buffer_offset) = Self::type_and_offset_of(slf, attr)?;
+        let type_size = attr_type
+            .call_method0("__packed_size__")?
+            .downcast::<PyLong>()?
+            .extract::<usize>()?;
+
+        Ok(attr_type.call_method1(
+            "__new__",
+            (
+                attr_type,
+                &slf.borrow().buffer.borrow()[buffer_offset..buffer_offset + type_size],
+            ),
+        )?)
+    }
+}
 
 #[pyclass(name = "_bool", subclass, weakref)]
 pub struct Bool(bool);
@@ -131,34 +255,53 @@ impl Int32 {
 }
 
 #[pyclass(name = "_i64", subclass, weakref)]
-pub struct Int64(i64);
+pub struct Int64{
+    buffer: RefCell<[u8; std::mem::size_of::<i64>()]>,
+    endianness: Endianness,
+}
 
 #[pymethods]
 impl Int64 {
+    const PACKED_SIZE: usize = std::mem::size_of::<i64>();
     #[new]
-    fn new(value: i64) -> Self {
-        Self(value)
-    }
-
-    fn __repr__(slf: &PyCell<Self>) -> PyResult<String> {
-        let class_name: &str = slf.get_type().name()?;
-        Ok(format!("{class_name}({})", slf.borrow().0))
-    }
-
-    fn __richcmp__(&self, other: &Self, op: CompareOp) -> PyResult<bool> {
-        match op {
-            CompareOp::Lt => Ok(self.0 < other.0),
-            CompareOp::Le => Ok(self.0 <= other.0),
-            CompareOp::Eq => Ok(self.0 == other.0),
-            CompareOp::Ne => Ok(self.0 != other.0),
-            CompareOp::Gt => Ok(self.0 > other.0),
-            CompareOp::Ge => Ok(self.0 >= other.0),
+    fn new(value: Option<&PyAny>, endianness: Option<Endianness>) -> Self {
+        let endianness = endianness.unwrap_or(Endianness::Native(Native()));
+        match value {
+            Some(value) => {
+                if let Ok(literal) = value.downcast::<PyLong>() {
+                    let literal = literal.extract::<i64>().unwrap();
+                    Self { buffer: RefCell::new([0_u8; std::mem::size_of::<i64>()]), endianness}
+                } else if let Ok(buffer) = value.downcast::<PyBytes>() {
+                    let buffer = buffer.extract::<&[u8]>().unwrap();
+                    Self{buffer: RefCell::new(*buffer_alias::<{std::mem::size_of::<i64>()}>(buffer)), endianness}
+                }
+                else {
+                    Self { buffer: RefCell::new([0_u8; std::mem::size_of::<i64>()]), endianness}
+                }
+            }
+            None => Self { buffer: RefCell::new([0_u8; std::mem::size_of::<i64>()]), endianness}
         }
     }
 
+    // fn __repr__(slf: &PyCell<Self>) -> PyResult<String> {
+    //     let class_name: &str = slf.get_type().name()?;
+    //     Ok(format!("{class_name}({})", slf.borrow().0))
+    // }
+
+    // fn __richcmp__(&self, other: &Self, op: CompareOp) -> PyResult<bool> {
+    //     match op {
+    //         CompareOp::Lt => Ok(self.0 < other.0),
+    //         CompareOp::Le => Ok(self.0 <= other.0),
+    //         CompareOp::Eq => Ok(self.0 == other.0),
+    //         CompareOp::Ne => Ok(self.0 != other.0),
+    //         CompareOp::Gt => Ok(self.0 > other.0),
+    //         CompareOp::Ge => Ok(self.0 >= other.0),
+    //     }
+    // }
+
     #[classmethod]
     fn __packed_size__(_cls: &PyType) -> PyResult<usize> {
-        Ok(std::mem::size_of::<Self>())
+        Ok(Self::PACKED_SIZE)
     }
 }
 
